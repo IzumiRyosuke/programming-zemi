42cc87c548389a8cee7cf263a103d6d5ae7ce29d
IzumiRyosuke <ryosuke.i1233@gmail.com>
2021-09-30 11:47:46 +0000

    プログラミングゼミでhello worldを表示してみたよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # 以下、追加コード
+  get 'hello-world' => "foo#hello"
 end
