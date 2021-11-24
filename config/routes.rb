879189dbae50fb1c6b6087a6922d02bfa87a3aa0
root <ryosuke.i1233@gmail.com>
2021-11-24 08:30:47 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get  'signup' => "users#new"
   resources :tweets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # 以下、追加コード
-  get 'hello-world' => "foo#hello"
 end
